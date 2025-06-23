835a5c1bff92dd1ace2363390f797b58e96a1204
AndreMoreira00 <andremoreira102030@gmail.com>
2025-06-23 11:19:44 -0300

    main

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    typedef enum {
+        BLOQUEADO,
+        DESBLOQUEADO
+    } ProcessState;
+
+    srand(time(NULL));
+
+    int random_time = rand()%10;
+    printf("%d", random_time);
+    
+    int random_class = rand()%2;
+    printf("%d", random_class);
+
+
+
     return 0;
 }
