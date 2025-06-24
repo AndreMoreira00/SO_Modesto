f3ef136ad897b998df2c945ee1b6451c37796617
Henrique Marangoni <henrique.1108@hotmail.com>
2025-06-23 21:46:43 -0300

    rr_scheduler

--- rr_scheduler.h
+++ rr_scheduler.h
@@ -1,10 +1,53 @@
-#ifndef RR_SCHEDULER_H
-#define RR_SCHEDULER_H
+#include "process.h"
+#include <stddef.h>
 
-#include "scheduler.h"
+// Remove um processoa da fila de prontos
+// Retorna 1 se removeu / 0 se não encontrou.
+static int remove_from_queue(Scheduler *sched, Process *p)
+{
+    if (sched->ready_queue == NULL || p == NULL)
+    {
+        return 0;
+    }
 
-// Funções específicas do Round Robin
-void handle_blocked_process(Scheduler *sched, Process *p);
-void handle_terminated_process(Scheduler *sched, Process *p);
+    Process *prev = sched->ready_queue;
+    while (prev->next != p && prev != sched->ready_queue->next)
+    {
+        prev = prev->next;
+    }
 
-#endif
+    if (prev->next == p)
+    {
+        if (p == p->next)
+        {
+            sched->ready_queue = NULL;
+        }
+        else
+        {
+            prev->next = p->next;
+            if (sched->ready_queue == p)
+            {
+                sched->ready_queue = prev;
+            }
+        }
+        p->next = NULL;
+        return 1;
+    }
+
+    return 0;
+}
+
+// Remove um processo bloqueado da fila de prontos
+void handle_blocked_process(Scheduler *sched, Process *p)
+{
+    remove_from_queue(sched, p);
+}
+
+// Remove um processo terminado da fila
+void handle_terminated_process(Scheduler *sched, Process *p)
+{
+    if (remove_from_queue(sched, p))
+    {
+        destroy_process(p);
+    }
+}
